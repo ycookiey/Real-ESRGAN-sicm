014cf6ec2824146a090fc49e8bb433056619aa3d
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-03-19 17:35:48 +0900

    update:新しいデータセットクラスRealESRGANCSVDatasetを追加

--- realesrgan/data/realesrgan_dataset.py
+++ realesrgan/data/realesrgan_dataset.py
@@ -214,3 +214,153 @@ class RealESRGANDataset(data.Dataset):
 
     def __len__(self):
         return len(self.paths)
+
+import pandas as pd
+
+
+@DATASET_REGISTRY.register()
+class RealESRGANCSVDataset(data.Dataset):
+    def __init__(self, opt):
+        super(RealESRGANCSVDataset, self).__init__()
+        self.opt = opt
+        self.file_client = None
+        self.io_backend_opt = opt["io_backend"]
+        self.gt_folder = opt["dataroot_gt"]
+
+        with open(self.opt["meta_info"]) as fin:
+            paths = [line.strip().split(" ")[0] for line in fin]
+            self.paths = [os.path.join(self.gt_folder, v) for v in paths]
+
+        self.blur_kernel_size = opt["blur_kernel_size"]
+        self.kernel_list = opt["kernel_list"]
+        self.kernel_prob = opt["kernel_prob"]
+        self.blur_sigma = opt["blur_sigma"]
+        self.betag_range = opt["betag_range"]
+        self.betap_range = opt["betap_range"]
+        self.sinc_prob = opt["sinc_prob"]
+
+        self.blur_kernel_size2 = opt["blur_kernel_size2"]
+        self.kernel_list2 = opt["kernel_list2"]
+        self.kernel_prob2 = opt["kernel_prob2"]
+        self.blur_sigma2 = opt["blur_sigma2"]
+        self.betag_range2 = opt["betag_range2"]
+        self.betap_range2 = opt["betap_range2"]
+        self.sinc_prob2 = opt["sinc_prob2"]
+
+        self.final_sinc_prob = opt["final_sinc_prob"]
+
+        self.kernel_range = [2 * v + 1 for v in range(3, 11)]
+        # TODO: kernel range is now hard-coded, should be in the configure file
+        self.pulse_tensor = torch.zeros(21, 21).float()
+        self.pulse_tensor[10, 10] = 1
+
+    def __getitem__(self, index):
+        if self.file_client is None:
+            self.file_client = FileClient(
+                self.io_backend_opt.pop("type"), **self.io_backend_opt
+            )
+
+        # -------------------------------- Load gt images -------------------------------- #
+        gt_path = self.paths[index]
+        try:
+            data = pd.read_csv(gt_path, header=None).to_numpy()
+            img_gt = data.astype(np.float32)
+
+            img_gt = np.stack([img_gt] * 3, axis=2).copy()
+        except Exception as e:
+            logger = get_root_logger()
+            logger.warning(f"csv read error: {e}")
+            index = random.randint(0, self.__len__())
+            return self.__getitem__(index)
+
+        # -------------------- Do augmentation for training: flip, rotation -------------------- #
+        img_gt = augment(img_gt, self.opt["use_hflip"], self.opt["use_rot"])
+
+        # TODO: 128 is hard-coded. You may change it accordingly
+        h, w = img_gt.shape[0:2]
+        crop_pad_size = 128
+        if h < crop_pad_size or w < crop_pad_size:
+            pad_h = max(0, crop_pad_size - h)
+            pad_w = max(0, crop_pad_size - w)
+            img_gt = cv2.copyMakeBorder(
+                img_gt, 0, pad_h, 0, pad_w, cv2.BORDER_REFLECT_101
+            )
+
+        if img_gt.shape[0] > crop_pad_size or img_gt.shape[1] > crop_pad_size:
+            h, w = img_gt.shape[0:2]
+            top = random.randint(0, h - crop_pad_size)
+            left = random.randint(0, w - crop_pad_size)
+            img_gt = img_gt[top : top + crop_pad_size, left : left + crop_pad_size, ...]
+
+        # ------------------------ Generate kernels (used in the first degradation) ------------------------ #
+        kernel_size = random.choice(self.kernel_range)
+        if np.random.uniform() < self.opt["sinc_prob"]:
+            if kernel_size < 13:
+                omega_c = np.random.uniform(np.pi / 3, np.pi)
+            else:
+                omega_c = np.random.uniform(np.pi / 5, np.pi)
+            kernel = circular_lowpass_kernel(omega_c, kernel_size, pad_to=False)
+        else:
+            kernel = random_mixed_kernels(
+                self.kernel_list,
+                self.kernel_prob,
+                kernel_size,
+                self.blur_sigma,
+                self.blur_sigma,
+                [-math.pi, math.pi],
+                self.betag_range,
+                self.betap_range,
+                noise_range=None,
+            )
+
+        pad_size = (21 - kernel_size) // 2
+        kernel = np.pad(kernel, ((pad_size, pad_size), (pad_size, pad_size)))
+
+        # ------------------------ Generate kernels (used in the second degradation) ------------------------ #
+        kernel_size = random.choice(self.kernel_range)
+        if np.random.uniform() < self.opt["sinc_prob2"]:
+            if kernel_size < 13:
+                omega_c = np.random.uniform(np.pi / 3, np.pi)
+            else:
+                omega_c = np.random.uniform(np.pi / 5, np.pi)
+            kernel2 = circular_lowpass_kernel(omega_c, kernel_size, pad_to=False)
+        else:
+            kernel2 = random_mixed_kernels(
+                self.kernel_list2,
+                self.kernel_prob2,
+                kernel_size,
+                self.blur_sigma2,
+                self.blur_sigma2,
+                [-math.pi, math.pi],
+                self.betag_range2,
+                self.betap_range2,
+                noise_range=None,
+            )
+
+        pad_size = (21 - kernel_size) // 2
+        kernel2 = np.pad(kernel2, ((pad_size, pad_size), (pad_size, pad_size)))
+
+        # ------------------------------------- the final sinc kernel ------------------------------------- #
+        if np.random.uniform() < self.opt["final_sinc_prob"]:
+            kernel_size = random.choice(self.kernel_range)
+            omega_c = np.random.uniform(np.pi / 3, np.pi)
+            sinc_kernel = circular_lowpass_kernel(omega_c, kernel_size, pad_to=21)
+            sinc_kernel = torch.FloatTensor(sinc_kernel)
+        else:
+            sinc_kernel = self.pulse_tensor
+
+        img_gt = img2tensor([img_gt], bgr2rgb=True, float32=True)[0]
+        kernel = torch.FloatTensor(kernel)
+        kernel2 = torch.FloatTensor(kernel2)
+
+        return_d = {
+            "gt": img_gt,
+            "kernel1": kernel,
+            "kernel2": kernel2,
+            "sinc_kernel": sinc_kernel,
+            "gt_path": gt_path,
+        }
+        return return_d
+
+    def __len__(self):
+        return len(self.paths)
